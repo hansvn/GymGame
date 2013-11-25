e7e1afe75a1cf67fac428647f1027b7df5a23c0b
Thomas Van Roost <thomasvanroost@gmail.com>
2013-11-25 14:06:51 +0100

    Quizmaster/backend views en controllers aangemaakt

---
+++ GymGame/Controllers/QuizMasterController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GymGame.Controllers
+{
+    public class QuizMasterController : Controller
+    {
+        // This controller will handle backend features for administrators.
+        // New quizes can be created and managed.
+        // GET: /QuizMaster/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult New()
+        {
+            return View();
+        }
+
+        public ActionResult Manage()
+        {
+            return View();
+        }
+    }
+}
