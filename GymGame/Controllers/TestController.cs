ace673e1b90f7b46754a0945f453b9d6e5faa095
Thomas Van Roost <thomasvanroost@gmail.com>
2013-11-27 11:51:41 +0100

    commit voor juiste csproj file 3de poging

---
+++ GymGame/Controllers/TestController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GymGame.Controllers
+{
+    public class TestController : Controller
+    {
+        //
+        // GET: /Test/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public string Welcome(String name, int aantal = 0)
+        {
+            return HttpUtility.HtmlEncode("Hello " + name + ", Je was hier " + aantal + " keer");
+        }
+
+    }
+}
