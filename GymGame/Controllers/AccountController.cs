d05fcd0e872f9a4893f85161960b9009060f09d5
hansvn <hans_vn@hotmail.com>
2013-11-27 13:12:14 +0100

    nuget facebook client en accountcontroller
    met nuget de facebook client geïnstalleerd en accountcontroller
    aangemaakt (met basic functionaliteit met post enz.)
    login view aangemaakt met klein facebook knopje (voor testen, moet in
    master layout komen)

---
+++ GymGame/Controllers/AccountController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GymGame.Models;
+
+namespace GymGame.Controllers
+{
+    public class AccountController : Controller
+    {
+        //
+        // GET: /Account/
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(FormCollection f)
+        {
+            var accessToken = f["accessToken"];
+
+            var client = new Facebook.FacebookClient(accessToken);
+            dynamic result = client.Get("me", new { fields = "name, id" });
+
+            UserModel um = new UserModel();
+
+            ViewBag.status = "loggedIn";
+            return View();
+
+        }
+
+    }
+}
