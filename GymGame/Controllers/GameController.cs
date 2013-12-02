20786c188d8b7448b6eca9844f31447958b6382d
Thomas Van Roost <thomasvanroost@gmail.com>
2013-12-02 13:42:32 +0100

    aanpassingen view en controller bootstrap files toegevoegd

--- GymGame/Controllers/GameController.cs
+++ GymGame/Controllers/GameController.cs
@@ -11,10 +11,23 @@ namespace GymGame.Controllers
        
         // GET: /Game/
 
-        public ActionResult Index()
+        public ActionResult Index(String Id)
         {
+            // Op basis van wat er werd ingegeven achter Game/... wordt er een bepaalde quiz geselecteerd.
+            // checken of de gebruiker al is ingelogd. Indien niet -> /login -> 
+            ViewBag.id = Id;
+            // is er een user sessie? En wie is de gebruiker?
+            ViewBag.username = "Willy"; // test Willy hardcoded.
             return View();
         }
+
+
+        public void StartGame() 
+        {
+            
+        }
+
+    
 
     }
 }
--- GymGame/Controllers/QuizMasterController.cs
+++ GymGame/Controllers/QuizMasterController.cs
@@ -3,11 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using GymGame.Models;
 
 namespace GymGame.Controllers
 {
-    
-
 
     public class QuizMasterController : Controller
     {
@@ -20,28 +19,66 @@ namespace GymGame.Controllers
             return View();
         }
 
+        public ActionResult New()
+        {
+            return View();
+        }
+
         [HttpPost]
         public ActionResult New(FormCollection f)
         {
-            string namegame = f["naam"];
+            GymGameModelDataContext dc = new GymGameModelDataContext();
+            // uit post halen: gameName, question, a1, a2, a3
+            var gamename = f["gameName"];
+            var question = f["question"];
+            var answer1 = f["a1"];
+            var answer2 = f["a2"];
+            var answer3 = f["a3"];
+            // wegschrijven
+
+
+
+            //  Quiz q = new Quiz();
+
             return View();
         }
 
         public ActionResult Manage()
         {
-     
+            GymGameModelDataContext dc = new GymGameModelDataContext();
+            // user komt normaal gezien uit de sessievar.
+            User u = new User();
+            u.User_Id = 1;
+            QuizMasterModel qmm = new QuizMasterModel();
+            List<Quiz> Allquizzes = qmm.getAllQuizzes(u);
+           
+            ViewBag.quizzes = Allquizzes;
             return View();
         }
 
-        public ActionResult Edit(int id)
+
+        
+
+        [HttpPost]
+        public ActionResult Edit(String quiz_Id)
         {
-
-            ViewBag.questions = "";
-            return View();
+            // alle velden van een bepaalde quiz opvragen.
+            // en de velden hiermee vullen.
+            var Quiz = new Quiz { Location = "IMDEVENT" + quiz_Id };
+            return View(Quiz);
         }
+
+        public String Delete(String quiz_Id)
+        {
+            // uit datadank verwijderen, maar hoe?
+            return quiz_Id + " uit de databank verwijderd" + "<a href='/Quizmaster/manage'> terug </a>";
+        }
+
 
         public ActionResult Start()
         {
+            // een quiz starten. Timer starten.. 
+
             return View();
         }
     }
